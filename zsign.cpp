824a1c8bdc9c96da570bd9c0103bbc9e265bf07a
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-12-05 20:18:12 -0500

    Update zsign.cpp

--- zsign.cpp
+++ zsign.cpp
@@ -28,32 +28,6 @@ const struct option options[] = {
 	{"help", no_argument, NULL, 'h'},
 	{}};
 
-int usage()
-{
-	ZLog::Print("Usage: zsign [-options] [-k privkey.pem] [-m dev.prov] [-o output.ipa] file|folder\n");
-	ZLog::Print("options:\n");
-	ZLog::Print("-k, --pkey\t\tPath to private key or p12 file. (PEM or DER format)\n");
-	ZLog::Print("-m, --prov\t\tPath to mobile provisioning profile.\n");
-	ZLog::Print("-c, --cert\t\tPath to certificate file. (PEM or DER format)\n");
-	ZLog::Print("-d, --debug\t\tGenerate debug output files. (.zsign_debug folder)\n");
-	ZLog::Print("-f, --force\t\tForce sign without cache when signing folder.\n");
-	ZLog::Print("-o, --output\t\tPath to output ipa file.\n");
-	ZLog::Print("-p, --password\t\tPassword for private key or p12 file.\n");
-	ZLog::Print("-b, --bundle_id\t\tNew bundle id to change.\n");
-	ZLog::Print("-n, --bundle_name\tNew bundle name to change.\n");
-	ZLog::Print("-r, --bundle_version\tNew bundle version to change.\n");
-	ZLog::Print("-e, --entitlements\tNew entitlements to change.\n");
-	ZLog::Print("-z, --zip_level\t\tCompressed level when output the ipa file. (0-9)\n");
-	ZLog::Print("-l, --dylib\t\tPath to inject dylib file.\n");
-	ZLog::Print("-w, --weak\t\tInject dylib as LC_LOAD_WEAK_DYLIB.\n");
-	ZLog::Print("-i, --install\t\tInstall ipa file using ideviceinstaller command for test.\n");
-	ZLog::Print("-q, --quiet\t\tQuiet operation.\n");
-	ZLog::Print("-v, --version\t\tShows version.\n");
-	ZLog::Print("-h, --help\t\tShows help (this message).\n");
-
-	return -1;
-}
-
 int zsign(int argc, char *argv[])
 {
 	ZTimer gtimer;
@@ -62,7 +36,6 @@ int zsign(int argc, char *argv[])
 	bool bInstall = false;
 	bool bWeakInject = false;
 	uint32_t uZipLevel = 0;
-
 	string strCertFile;
 	string strPKeyFile;
 	string strProvFile;
@@ -73,7 +46,6 @@ int zsign(int argc, char *argv[])
 	string strOutputFile;
 	string strDisplayName;
 	string strEntitlementsFile;
-
 	int opt = 0;
 	int argslot = -1;
 	while (-1 != (opt = getopt_long(argc, argv, "dfvhc:k:m:o:ip:e:b:n:z:ql:w", options, &argslot)))
@@ -93,24 +65,8 @@ int zsign(int argc, char *argv[])
 			strPassword = optarg;
 			break;
 		}
-
 		ZLog::DebugV(">>> Option:\t-%c, %s\n", opt, optarg);
 	}
-
-	if (optind >= argc)
-	{
-		return usage();
-	}
-
-	if (ZLog::IsDebug())
-	{
-		CreateFolder("./.zsign_debug");
-		for (int i = optind; i < argc; i++)
-		{
-			ZLog::DebugV(">>> Argument:\t%s\n", argv[i]);
-		}
-	}
-
 	string strPath = GetCanonicalizePath(argv[optind]);
 	if (!IsFileExists(strPath.c_str()))
 	{
