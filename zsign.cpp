06eca922077e16f2cafb9d5d542e7ad936de646c
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-12-07 11:45:13 -0500

    Update zsign.cpp

--- zsign.cpp
+++ zsign.cpp
@@ -7,38 +7,18 @@
 extern "C" int zsign(char* appPath, char* p12Path, char* provPath, char* pass);
 
 int zsign(char* appPath, char* p12Path, char* provPath, char* pass) {
-	ZTimer gtimer;
-	bool bForce = false;
-	bool bInstall = false;
-	bool bWeakInject = false;
-	uint32_t uZipLevel = 0;
-	string strCertFile;
-	string strPKeyFile = p12Path;
-	string strProvFile = provPath;
-	string strPassword = pass;
-	string strBundleId;
-	string strBundleVersion;
-	string strDyLibFile;
-	string strOutputFile;
-	string strDisplayName;
-	string strEntitlementsFile;
-	string strPath = appPath;
-	if (!IsFileExists(strPath.c_str()))
-	{
-		ZLog::ErrorV(">>> Invalid Path! %s\n", strPath.c_str());
-		return -1;
-	}
-	ZTimer timer;
-	ZSignAsset zSignAsset;
-	if (!zSignAsset.Init(strCertFile, strPKeyFile, strProvFile, strEntitlementsFile, strPassword)) {
-	    return -1;
-	}
-	bool bEnableCache = true;
-	string strFolder = strPath;
-	timer.Reset();
-	ZAppBundle bundle;
-	bool bRet = bundle.SignFolder(&zSignAsset, strFolder, strBundleId, strBundleVersion, strDisplayName, strDyLibFile, bForce, bWeakInject, bEnableCache);
-	timer.PrintResult(bRet, ">>> Signed %s!", bRet ? "OK" : "Failed");
-	gtimer.Print(">>> Done.");
-	return bRet ? 0 : -1;
+    bool bForce = false;
+    bool bWeakInject = false;
+    if (!IsFileExists(appPath)) {
+        ZLog::ErrorV(">>> Invalid Path! %s\n", appPath);
+        return -1;
+    }
+    ZSignAsset zSignAsset;
+    if (!zSignAsset.Init("", p12Path, provPath, "", pass)) {
+        return -1;
+    }
+    bool bEnableCache = true;
+    ZAppBundle bundle;
+    bool bRet = bundle.SignFolder(&zSignAsset, appPath, "", "", "", "", bForce, bWeakInject, bEnableCache);
+    return 0;
 }
