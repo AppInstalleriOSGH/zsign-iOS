456a7938bf7312319ed70e9c36f9be7aaeebe075
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-12-07 15:18:34 -0500

    Update zsign.cpp

--- zsign.cpp
+++ zsign.cpp
@@ -1,14 +1,9 @@
 #include "common/common.h"
-#include "common/json.h"
-#include "openssl.h"
-#include "macho.h"
 #include "bundle.h"
 
 extern "C" int zsign(char* appPath, char* p12Path, char* provPath, char* pass, char* bundleID, char* bundleVersion, char* displayName, char* tweakDylib);
 
 int zsign(char* appPath, char* p12Path, char* provPath, char* pass, char* bundleID, char* bundleVersion, char* displayName, char* tweakDylib) {
-    bool bForce = false;
-    bool bWeakInject = !(std::string str(tweakDylib)).empty();
     if (!IsFileExists(appPath)) {
         ZLog::ErrorV(">>> Invalid Path! %s\n", appPath);
         return -1;
@@ -17,8 +12,7 @@ int zsign(char* appPath, char* p12Path, char* provPath, char* pass, char* bundle
     if (!zSignAsset.Init("", p12Path, provPath, "", pass)) {
         return -1;
     }
-    bool bEnableCache = true;
     ZAppBundle bundle;
-    bool bRet = bundle.SignFolder(&zSignAsset, appPath, bundleID, bundleVersion, displayName, tweakDylib, bForce, bWeakInject, bEnableCache);
+    bool bRet = bundle.SignFolder(&zSignAsset, appPath, bundleID, bundleVersion, displayName, tweakDylib, false, true, true);
     return bRet ? 0 : 1;
 }
