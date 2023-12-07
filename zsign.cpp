e6d4d9e91b5f5c45d9a0ebc20dd4832cf2602493
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-12-07 11:41:40 -0500

    Update zsign.cpp

--- zsign.cpp
+++ zsign.cpp
@@ -3,30 +3,6 @@
 #include "openssl.h"
 #include "macho.h"
 #include "bundle.h"
-#include <libgen.h>
-#include <dirent.h>
-#include <getopt.h>
-
-const struct option options[] = {
-	{"debug", no_argument, NULL, 'd'},
-	{"force", no_argument, NULL, 'f'},
-	{"verbose", no_argument, NULL, 'v'},
-	{"cert", required_argument, NULL, 'c'},
-	{"pkey", required_argument, NULL, 'k'},
-	{"prov", required_argument, NULL, 'm'},
-	{"password", required_argument, NULL, 'p'},
-	{"bundle_id", required_argument, NULL, 'b'},
-	{"bundle_name", required_argument, NULL, 'n'},
-	{"bundle_version", required_argument, NULL, 'r'},
-	{"entitlements", required_argument, NULL, 'e'},
-	{"output", required_argument, NULL, 'o'},
-	{"zip_level", required_argument, NULL, 'z'},
-	{"dylib", required_argument, NULL, 'l'},
-	{"weak", no_argument, NULL, 'w'},
-	{"install", no_argument, NULL, 'i'},
-	{"quiet", no_argument, NULL, 'q'},
-	{"help", no_argument, NULL, 'h'},
-	{}};
 
 extern "C" int zsign(char* appPath, char* p12Path, char* provPath, char* pass);
 
